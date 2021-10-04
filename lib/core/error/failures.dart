68aeb5e8fcfdc9ba8328bd592bed646539650358
Afnanabu <89046363+Afnanabu@users.noreply.github.com>
2021-10-04 16:02:03 +0300

    Add files via upload

---
+++ lib/core/error/failures.dart
@@ -0,0 +1,9 @@
+import 'package:equatable/equatable.dart';
+
+ class Errors extends Equatable{
+  Errors([List proparity=const<dynamic>[]]) ;
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [Errors([])];
+}
