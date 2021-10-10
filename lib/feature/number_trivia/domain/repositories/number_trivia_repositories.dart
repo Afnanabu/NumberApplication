ebf91c3148d7ec6e471ce4c86ccf971095e317b2
Afnanabu <89046363+Afnanabu@users.noreply.github.com>
2021-10-10 09:56:46 +0300

    Delete lib directory

--- lib/feature/number_trivia/domain/repositories/number_trivia_repositories.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
- import 'package:numbertrivia/core/error/failures.dart';
-import 'package:numbertrivia/feature/number_trivia/domain/entities/number_trivia.dart';
-abstract class NumberTriviaRepository
-{
-  Future <Either<Failure,NumberTrivia>> getConcreteNumberTrivia(int number);
-  Future <Either<Failure,NumberTrivia>> getRandomNumberTrivia();
-}
